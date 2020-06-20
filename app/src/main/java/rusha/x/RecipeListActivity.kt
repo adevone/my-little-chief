28d94b331051ab5d41ecbd31746d066e6fb902ca
adevone <terehovks3@gmail.com>
2020-06-20 20:18:38 +0300

    Add favorite and recipe list layouts

---
+++ app/src/main/java/rusha/x/RecipeListActivity.kt
@@ -0,0 +1,13 @@
+package rusha.x
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecipeListActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recipe_list_activity)
+
+    }
+
+}
